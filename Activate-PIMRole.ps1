7168652b9cbfc770bb72886f62721681d2698aca
Krylov <stanislav.krylov@effem.com>
2022-08-01 13:09:47 +0300

    added check and istall modules function; another loop added to schedule more than one role in one go; replaced AzureRM with Az.Accounts

--- Activate-PIMRole.ps1
+++ Activate-PIMRole.ps1
@@ -1,11 +1,44 @@
-##
-# Do not Forget to install PS modules if you use it for the first time!
-# To do so uncomment two string bellow 
-##
+function CheckInstall-Module
+{
+	<#
+	.SYNOPSIS
+	This Funciton check if you have Module installed and tries to install it if it's not.
+	.DESCRIPTION
+	ModuleName is a required parameter
+	.PARAMETER ModuleName
+	Required paramter. ModuleName to check whther this Module is installed.
+	#>
+	param(
+		[Parameter(Mandatory=$true)]
+		[string]$ModuleName
+	)
 
-#Install-Module -Name AzureRM -AllowClobber
-#Install-Module AzureADPreview # Note PIM commandlets are currently available only in preview version
+	if (-not (Get-Module -ListAvailable $ModuleName).path){
+		Write-Host "The $($ModuleName) Module is not installed, we will try to install it now" -ForegroundColor Yellow
+		#write-Host "This will only work if you are running this script as Local Administrator" -ForegroundColor Yellow
+		Write-Host
+		Install-Module -Name $ModuleName
+	}
+	# TODO: Add Error handler to see if the ModuleName exists and can be found in PSGallery
+	# TODO: Add Check for local Admin rights required to install ModuleName
 
+	<#	Local Admin check part in case it required to install some module. TODO: Check if it's required.
+	# Check if the script runs in an local Administrator context
+    If ($(([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) -eq $True)
+        {Install-Module -Name Microsoft.Azure.ActiveDirectory.PIM.PSModule} 
+    
+    # Exit if PowerShell if not run as admin
+    Else
+	{
+		Write-Host "You are not running the script as Local Admin. The script will exit now" -ForegroundColor Yellow
+    	Exit
+	}
+#>
+}
+# Checking for modules required
+# TODO: Check if AzureAD isntalled - might generate conflict as you can have either AzureAD or AzureADPreview installed accotding to MS docs: https://docs.microsoft.com/en-us/powershell/azure/active-directory/install-adv2?view=azureadps-2.0#installing-the-azure-ad-module
+CheckInstall-Module -ModuleName Az.Accounts
+CheckInstall-Module -ModuleName AzureADPreview # NOTE: PIM commandlets are currently available only in preview version
 
 $daysNum = 30	# max number of scheduled activations is 30
 $Duration = 10	# time in role in hours per activation, max value may vary; you can check it in PIM on azure portal
@@ -19,8 +52,9 @@ $startDayTime = (Get-Date).AddDays(1).Date.AddHours(9)
 $skipWeekends = $TRUE
 
 # Login and fetch some service variables
-Login-AzureRmAccount
-$currentAzureContext = Get-AzureRmContext
+Import-Module Az
+Connect-AzAccount
+$currentAzureContext = Get-AzContext
 $TenantID = $currentAzureContext.Tenant.Id
 $accountId = $currentAzureContext.Account.Id
 Connect-AzureAD -TenantId $TenantID -AccountId $accountId
@@ -32,45 +66,50 @@ $rolestable = @(); $roles = @()
 $assignments = Get-AzureADMSPrivilegedRoleAssignment -ProviderId "aadRoles" -ResourceId $TenantID -Filter "subjectId eq '$($user.ObjectId)'" | select RoleDefinitionId -Unique
 foreach ($each in $assignments) { $roles += Get-AzureADDirectoryRoleTemplate | ? { $_.ObjectID -eq $each.RoleDefinitionId }}
 $roles | % {$row = "" | Select Index, Role; $row.Role = $_.DisplayName; $row.Index = $roles.IndexOf($_); $rolestable += $row}
-Write $rolestable
 
-$selectedRole = [int](Read-Host "Enter Index to select activating Role")
+$repeat = "y" # initial repeat flag for first iteration of following while loop
+while ($repeat -eq "y"){ # while loop to schedule multiple roles one by one if required
+	Write $rolestable
 
-$role = $roles | ? { $_.DisplayName -eq $rolestable[$selectedRole].Role }
+	$selectedRole = [int](Read-Host "Enter Index to select activating Role")
 
-$reason = Read-Host "Enter reason (max 500 characters)"
+	$role = $roles | ? { $_.DisplayName -eq $rolestable[$selectedRole].Role }
 
-# setting up start date and the last date
-$StartDate = [DateTime](Get-Date -Date $startDayTime -Format "dddd MM/dd/yyyy HH:mm")
-$LastDate = $StartDate.AddDays($daysNum)
+	$reason = Read-Host "Enter reason (max 500 characters)"
 
-# here's where all the magic happens
-While($StartDate -ne $LastDate)
-{
+	# setting up start date and the last date
+	$StartDate = [DateTime](Get-Date -Date $startDayTime -Format "dddd MM/dd/yyyy HH:mm")
+	$LastDate = $StartDate.AddDays($daysNum)
 
-	$schedule = New-Object Microsoft.Open.MSGraph.Model.AzureADMSPrivilegedSchedule
-	$schedule.Type = "Once"
-	$schedule.StartDateTime = $StartDate.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ") 
-	$schedule.endDateTime = $schedule.StartDateTime.AddHours(10)
-	try 
+	# here's where all the magic happens
+	While($StartDate -ne $LastDate)
 	{
-		Open-AzureADMSPrivilegedRoleAssignmentRequest `
-			-ProviderId 'aadRoles' `
-			-ResourceId $TenantID `
-			-RoleDefinitionId $role.ObjectId `
-			-SubjectId $user.ObjectId `
-			-Type 'UserAdd' `
-			-AssignmentState 'Active' `
-			-schedule $schedule `
-			-reason $reason
-	} catch { Write-Warning $_ ; break}
 
-	If ( $skipWeekends -and ( $StartDate.DayOfWeek.value__ -eq 5 ) ) 
-    {
-        "$StartDate TGIF; Have a nice weekend without PIM roles ;)";
-        $StartDate = $StartDate.AddDays(3)
-        Continue 
-    } else {
-		$StartDate = $StartDate.AddDays(1)
+		$schedule = New-Object Microsoft.Open.MSGraph.Model.AzureADMSPrivilegedSchedule
+		$schedule.Type = "Once"
+		$schedule.StartDateTime = $StartDate.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ") 
+		$schedule.endDateTime = $schedule.StartDateTime.AddHours(10)
+		try 
+		{
+			Open-AzureADMSPrivilegedRoleAssignmentRequest `
+				-ProviderId 'aadRoles' `
+				-ResourceId $TenantID `
+				-RoleDefinitionId $role.ObjectId `
+				-SubjectId $user.ObjectId `
+				-Type 'UserAdd' `
+				-AssignmentState 'Active' `
+				-schedule $schedule `
+				-reason $reason
+		} catch { Write-Warning $_ ; break}
+
+		If ( $skipWeekends -and ( $StartDate.DayOfWeek.value__ -eq 5 ) ) 
+		{
+			"$StartDate TGIF; Have a nice weekend without PIM roles ;)";
+			$StartDate = $StartDate.AddDays(3)
+			Continue 
+		} else {
+			$StartDate = $StartDate.AddDays(1)
+		}
 	}
+	$repeat = Read-Host "Enter `"y`" if you want to schedule another role. To exit hit Enter"
 }
